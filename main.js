cd6de00cac5c05510b9582e996808c0a090d75aa
Hajime <hajime0316@s.okayama-u.ac.jp>
2020-11-23 11:15:42 +0900

    Add LED blinking

--- main.js
+++ main.js
@@ -6,6 +6,14 @@ async function main() {
     var port = i2cAccess.ports.get(1);
     var mlx = new MLX90614(port);
     await mlx.init();
+
+    const gpioAccess = await navigator.requestGPIOAccess();
+    const ledPort = gpioAccess.ports.get(26); // LED の GPIO ポート番号
+    await ledPort.export("out");
+
+    var blink_period_ms = 1000;
+    blink_led();
+
     while (1) {
       var otemp = await mlx.get_obj_temp();
       var atemp = await mlx.get_amb_temp();
@@ -13,20 +21,29 @@ async function main() {
         otemp.toFixed(2) + "degree";
       document.getElementById("amb_temperature").innerHTML =
         atemp.toFixed(2) + "degree";
+
+      // 点滅周期を変更する
+      blink_period_ms = (otemp - atemp) * 100;
       await sleep(200);
+    }
+
+    async function blink_led() {
+      while (1) {
+        if (blink_period_ms > 0) {
+          // LEDを点灯
+          await ledPort.write(1);
+          // 点滅周期の半分の時間止める
+          await sleep(blink_period_ms / 2);
+          // LEDを消灯
+          await ledPort.write(0);
+          // 点滅周期の半分の時間止める
+          await sleep(blink_period_ms / 2);
+        } else {
+          await sleep(10);
+        }
+      }
     }
   } catch (error) {
     console.error("error", error);
-  }
-
-  async function blink_led() {
-    // LEDを点灯
-
-    // 点滅周期の半分の時間止める
-    await sleep(blink_period / 2.0);
-    // LEDを消灯
-
-    // 点滅周期の半分の時間止める
-    await sleep(blink_period / 2.0);
   }
 }
